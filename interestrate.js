e0a67c13a1a5c47713978b8f34401dd8db55d0e3
sonomastea <sbstea@usf.edu>
2024-07-01 16:45:29 -0400

    Implement Interest Rate and Calculation Type

--- interestrate.js
+++ interestrate.js
@@ -1,2 +1,2 @@
-export function calculateInterest(principal, rate, time){ interest=
-principal*rate*time}
+export function calculateInterest(principal, rate, time){
+    return  interest= principal*rate*time}
